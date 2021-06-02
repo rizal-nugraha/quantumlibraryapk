e4b74afbfd14c1598b1f0aa450e96576cfb8f056
Rizal Nugraha <rizal.nugrah@gmail.com>
2021-06-02 20:30:30 +0700

    add Detail Activity

---
+++ app/src/main/java/id/kotlin/quantumlibrary/DetailQuantumActivity.kt
@@ -0,0 +1,10 @@
+package id.kotlin.quantumlibrary
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class DetailQuantumActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+    }
+}
