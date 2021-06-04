f8ecd0cff14f33ecd82bb16ad8988d1b4eece1d3
Rizal Nugraha <rizal.nugrah@gmail.com>
2021-06-04 19:20:52 +0700

    add data libraryData.kt

--- app/src/main/java/id/kotlin/quantumlibrary/libraryData.kt
+++ app/src/main/java/id/kotlin/quantumlibrary/libraryData.kt
@@ -5,22 +5,104 @@ import java.util.ArrayList
 internal object rizalData {
     var data = arrayOf(
         arrayOf(
-            "Book 1",
-            "Technology",
-            "https://upload.wikimedia.org/wikipedia/en/a/a2/The_7_Habits_of_Highly_Effective_People.jpg",
-            "Deskripsi",
-            "Tanggal",
-            "Tanggal",
-            "0"
+            "Atomic Habits",
+            "James Clear",
+            "https://ebooks.gramedia.com/ebook-covers/49184/image_highres/ID_AHPK2019MTH09AHP.jpg",
+            "Orang mengira ketika Anda ingin mengubah hidup, Anda perlu memikirkan hal-hal besar. Namun pakar kebiasaan terkenal kelas dunia James Clear telah menemukan sebuah cara\n" +
+                    "\n" +
+                    "lain. Ia tahu bahwa perubahan nyata berasal dari efek gabungan ratusan keputusan kecil—dari mengerjakan dua push-up sehari, bangun lima menit lebih awal, sampai menahan sebentar hasrat untuk menelepon.\n" +
+                    "\n" +
+                    "Ia menyebut semua tadi atomic habits.",
+            "Gramedia Pustaka Utama",
+            "1989",
+            "356"
         ), arrayOf(
-            "Book 2",
-            "Technology",
+            "The 7 Habits of Highly Effective People",
+            "Stephen R. Covey",
             "https://upload.wikimedia.org/wikipedia/en/a/a2/The_7_Habits_of_Highly_Effective_People.jpg",
-            "Deskripsi",
-            "Tanggal",
-            "Tanggal",
-            "0"
-        ),
+            "Tujuh Kebiasaan Manusia yang Sangat Efektif, untuk pertama kali terbit pada tahun 1989, adalah sebuah buku pengembangan diri yang ditulis oleh Stephen R. Covey. Buku ini sudah terjual lebih dari 15 juta kopi dalam 38 bahasa sejak publikasi pertamanya, yang ditandai dengan dirilisnya edisi ulang tahun ke-15 tahun 2004. Covey menyajikan pendekatan untuk menjadi efektif dalam mencapai tujuan dengan menyelaraskan diri pada apa yang dia sebut sebagai prinsip etika karakter yang menurutnya universal dan abadi.",
+            "Free Press",
+            "15 Agustus 1989",
+            "456"
+        ), arrayOf(
+            "Pengantar Pemrograman Kotlin",
+            "Jubilee Enterprise",
+            "https://ebooks.gramedia.com/ebook-covers/62949/image_highres/BLK_PPK2021874666.jpg",
+            "Bagi Anda yang ingin membuat aplikasi Android namun terlalu\n" +
+                    "\n" +
+                    "sulit memahami bahasa pemrograman Java, maka Kotlin adalah alternatif paling baik.\n" +
+                    "\n" +
+                    "Kotlin adalah bahasa pemrograman alternatif untuk pengembangan aplikasi Android dan aplikasi natives.",
+            "Elex Media Komputindo",
+            "10 March 2021.",
+            "86"
+        ), arrayOf(
+            "NLP: New Technology",
+            "NLP Comprehensive",
+            "https://ebooks.gramedia.com/ebook-covers/22968/big_covers/ID_HCO2015MTH04NNTE_B.jpg",
+            "NLP has already helped millions of people overcome fears, increase confidence, enrich relationships, and achieve greater sucess. Now the NLP Comprehensive Training Team has written a book that reveals how to use this breakthrough technology to .",
+            "Harper Collins",
+            "07 April 2015",
+            "248"
+        ), arrayOf(
+            "Smart Parenting Technology",
+            "Setiyo Widodo",
+            "https://ebooks.gramedia.com/ebook-covers/2212/big_covers/ID_EMK2013MTH02SPT_B.jpg",
+            "Anda menginginkan anak Anda berprestasi di sekolah, baik, rajin ibadah, menghormati Anda tetapi dengan berjalannya waktu, anda dibuat pusing oleh tingkah lakunya, cemas dengan masa depannya dan seolah Anda tidak mengenalnya lagi \"Rudi kok susah sekali dinasihati, main terus, susah sekali disuruh belajar!\" \"Anak nggak ada hormat-hormatnya sama orang tua. Dinasihati malah melawan!\" \"Tiap malam ke warnet, pulang jam 2 pagi.",
+            "Elex Media Komputindo",
+            "13 February 2013",
+            "320"
+        ), arrayOf(
+            "Information Technology Business Start-up",
+            "Yudha Yudhanto",
+            "https://ebooks.gramedia.com/ebook-covers/45602/image_highres/ID_ITBS2019MTH01ITBS.jpg",
+            "Saat ini bisnis dengan model start-up sedang digandrungi. Bisnis dengan model ini menjadi tren dan banyak orang yang mencoba peruntungan di model bisnis ini. Banyaknya kesuksesan yang dituai oleh para pelaku bisnis start-up menjadi patokan impian dan motivasi.",
+            "Elex Media Komputindo",
+            "02 January 2019",
+            "276"
+        ), arrayOf(
+            "Fish in the Water",
+            "Lee Chanhyuk",
+            "https://ebooks.gramedia.com/ebook-covers/64291/image_highres/gi0fsxi341ddzmv5oxxih6fjbwqxm0.jpg",
+            "Fish in the Water bertutur tentang kisah intens yang\n" +
+                    "\n" +
+                    "menjungkirbalikkan imajinasi, dengan komposisi kuat untuk\n" +
+                    "\n" +
+                    "menunjukkan pertentangan antara ambisi dan ketakutan,\n" +
+                    "\n" +
+                    "kebebasan dan kendali, kebahagiaan dari cinta dan luka dari\n" +
+                    "\n" +
+                    "kehilangan, serta arti kehidupan, terkadang dengan tenang\n" +
+                    "\n" +
+                    "dan terkadang dengan fantastis.",
+            "Gramedia Pustaka Utama",
+            "04 June 2021",
+            "187"
+        ), arrayOf(
+            "Masak 30 menit Bulan Ramadhan: Masak Tradisional",
+            "Sisca Soewitomo",
+            "https://ebooks.gramedia.com/ebook-covers/7151/big_covers/ID_GPU2013MTH08MMBRMT_B.jpg",
+            "Bagaimana cara kita agar tetap fit di bulan Ramadhan dan mampu melakukan segala aktivitas keseharian dengan tetap semangat? Salah satunya adalah dengan mengonsumsi makanan yang sehat dan bergizi saat sahur dan buka. Sahur penting bagi umat Muslim yang berpuasa untuk mendapatkan energi untuk beraktivitas sepanjang hari.",
+            "Gramedia Pustaka Utama",
+            "15 August 2013",
+            "80"
+        ), arrayOf(
+            "Kebebasan Finansial",
+            "Grant Sabatier",
+            "https://ebooks.gramedia.com/ebook-covers/64115/image_highres/BLK_KFCYSTUMSUYAB2021497088.jpg",
+            "Pada 2010, Grant Sabatier yang berusia 24 tahun hanya memiliki \$2,26 di rekening bank. Lima tahun kemudian, dia memiliki kekayaan bersih lebih dari \$1,25 juta. Pada usia 30 tahun, dia telah mencapai kebebasan finansial.",
+            "Gramedia Pustaka Utama",
+            "21 May 2021",
+            "450"
+        ), arrayOf(
+            "BThe Creative Thinking Handbook",
+            "Chris Griffiths, Melina Costi",
+            "https://ebooks.gramedia.com/ebook-covers/64040/image_highres/BLK_TCTH2021452527.jpg",
+            "Anda bisa bergerak dengan lancar dan dinamis melalui proses pemecahan masalah, terlepas dari apakah Anda bekerja sendiri atau dalam kolaborasi dengan orang lain. Anda juga bisa memilih untuk membaca tahapan atau bagian yang akan paling membantu Anda.",
+            "Elex Media Komputindo",
+            "17 May 2021",
+            "298"
+        )
     )
     val listData: ArrayList<rizal>
         get() {
